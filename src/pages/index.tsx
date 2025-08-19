4317987df4467982311f6cb56d2dd5b6b289066e
Renisson Silva <renissonx@gmail.com>
2025-08-18 23:07:39 -0300

    Configuração do padrão de temas do tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Card from '@/components/Card';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
+
+export default function Home() {
+  return (
+    <main
+      className={`grid place-content-center min-h-screen p-24 ${poppins.className}`}
+    >
+      <Card />
+    </main>
+  );
+}
